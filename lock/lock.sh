4f971789592d05cf21b6752a676404f1843b735c
chrism <chrism@rhrk.uni-kl.de>
2017-01-12 08:03:20 +0100

    + i3 config update + lockscript

---
+++ lock/lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -f -e -n -i /tmp/screen.png
